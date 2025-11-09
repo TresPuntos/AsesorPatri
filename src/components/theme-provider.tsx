d10416609971cb1a5f60de9ee5f539926647c7cb
TresPuntos <jordi@trespuntos.com>
2025-11-09 22:30:45 +0100

    fix: adjust theme provider types

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import type { ComponentProps } from "react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = ComponentProps<typeof NextThemeProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
